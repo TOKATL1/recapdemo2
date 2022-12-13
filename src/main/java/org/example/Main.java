e48e186024eea5aaad61be514c9ae86021203c9c
TOKATL1 <120321214+TOKATL1@users.noreply.github.com>
2022-12-13 22:46:58 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+int sayi= 8764;
+        int sayi2=987;
+int sayi3 = 8765;
+
+    int enBuyuk= sayi;
+
+
+    if(enBuyuk<sayi2){
+
+        enBuyuk=sayi2;
+    }
+      if (enBuyuk<sayi3){
+
+
+          enBuyuk=sayi3;
+
+
+
+          System.out.println("enBuyuk="+enBuyuk);
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        {
+}
+
+    }
+}
